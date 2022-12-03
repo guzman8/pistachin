64f1957fee579818b6f8676e87ff7ae0582c2a18
guzman8 <guzmangalofre@hotmail.com>
2022-12-03 12:30:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+
+
+// Dynamically populate the table with shopping list items.
+//Step below can be done via PHP and AJAX, too.
+function doShowAll() {
+    if (true) {
+        var key = "";
+        var list = "<tr><th>Item</th><th>Value</th></tr>\n";
+        var i = 0;
+        //For a more advanced feature, you can set a cap on max items in the cart.
+        for (i = 0; i <= localStorage.length-1; i++) {
+            key = localStorage.key(i);
+            list += "<tr><td>" + key + "</td>\n<td>"
+                    + localStorage.getItem(key) + "</td></tr>\n";
+        }
+        //If no item exists in the cart.
+        if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
+            list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+        }
+        //Bind the data to HTML table.
+        //You can use jQuery, too.
+        document.getElementById('list').innerHTML = list;
+    } else {
+        alert('Cannot save shopping list as your browser does not support HTML 5');
+    }
+}
+
+function SaveItem(name, data) {
+
+    localStorage.setItem(name, data);
+    doShowAll();
+
+}
+
+//Change an existing key-value in HTML5 storage.
+function ModifyItem() {
+    var name1 = document.forms.ShoppingList.name.value;
+    var data1 = document.forms.ShoppingList.data.value;
+    //check if name1 is already exists
+
+//Check if key exists.
+            if (localStorage.getItem(name1) !=null)
+            {
+              //update
+              localStorage.setItem(name1,data1);
+              document.forms.ShoppingList.data.value = localStorage.getItem(name1);
+            }
+
+    doShowAll();
+}
+
+function RemoveItem(){
+    var name=document.forms.ShoppingList.name.value;
+    document.forms.ShoppingList.data.value=localStorage.removeItem(name);
+    doShowAll();
+}
+
+function ClearAll() {
+    localStorage.clear();
+    doShowAll();
+}
+
