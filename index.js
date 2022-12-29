e1a38dbe97053f98358302771152a71adde79bad
Guzmán <usuario@usuarios-mbp.lan>
2022-12-29 18:33:35 +0100

    prueba5

--- index.js
+++ index.js
@@ -60,7 +60,22 @@ function ClearAll() {
     doShowAll();
 }
 
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
 
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+    console.log(visibility);
+    if (visibility === "false"){
+        primaryNav.setAttribute("data-visible",true);
+        navToggle.setAttribute("aria-expanded", true);
+    }else{
+        primaryNav.setAttribute("data-visible",false);
+        navToggle.setAttribute("aria-expanded",false);
+    }
+    const visibility2 = primaryNav.getAttribute("data-visible");
+    console.log(visibility2)
+});
 
 
 
