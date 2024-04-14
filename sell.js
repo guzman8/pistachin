bbd453f1736f56e15ef4ba25755ea456faac863e
guzman8 <guzmangalofre@hotmail.com>
2024-04-14 12:03:32 +0200

    Update sell.js
    tallas sudadera colors

--- sell.js
+++ sell.js
@@ -71,7 +71,7 @@ const Sizes = [["empty"],
     /*Sunny*/   ["empty",/*ranita*/["3 meses","3-6 meses","6-9 meses"],/*pelele*/["1-3 meses","3-6 meses"],/*vestido*/["2-3 años","3-4 años","4-5 años"],/*peto*/["12-18 meses","18-24 meses","4-5 años","5-6 años"]],
    /*Sailor*/   ["empty",/*pelel*/["3 meses","3-6 meses","6-9 meses"],/*vestido*/["1-3 meses","3-6 meses","6-9 meses","9-12 meses","12-18 meses","18-24 meses","2-3 años","3-4 años"],/*peto*/["9-12 meses","12-18 meses","18-24 meses","2-3 años","3-4 años","4-5 años"]],
    /*Bosque*/   ["empty",/*jersey*/["3-6 meses","6-9 meses","9-12 meses","12-18 meses","18-24 meses","2-3 años","3-4 años","4 años","6-8 años","8-10 años"]],
-   /*Colors*/   ["empty",/*sudadera infantil*/["2-3 años","3-4 años","4-6 años","6-8 años","8-10 años"],/*sudadera juvenil*/["S","M","L"]]];
+   /*Colors*/   ["empty",/*sudadera infantil*/["2-3 años","3-4 años","4-6 años","6-8 años","8-10 años"],/*sudadera juvenil*/["L"]]];
 const Prices = [["empty"],
                 ["empty","15","15","20","15"],
                 ["empty","10","10","12"],
